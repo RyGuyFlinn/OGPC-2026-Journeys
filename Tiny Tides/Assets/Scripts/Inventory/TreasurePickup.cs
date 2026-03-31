43130f9a3045b18decaadc074e9e0763c275bf72
KayphaB <197577@student.vansd.org>
2026-03-30 18:44:21 -0700

    gold attracted to player

--- Tiny Tides/Assets/Scripts/Inventory/TreasurePickup.cs
+++ Tiny Tides/Assets/Scripts/Inventory/TreasurePickup.cs
@@ -6,8 +6,34 @@ public class TreasurePickup : MonoBehaviour
 {
     public TreasureData treasureData;
     public int value;
+    public float speed;
+    public float attractionRadius;
+    public float playerRadius;
+    private GameObject player;
 
     public new AudioClip audio;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(player.transform.position, transform.position) - playerRadius;
+
+        if (distance <= attractionRadius)
+        {
+            if (distance < 1f)
+            {
+                transform.position = player.transform.position;
+            }
+            else
+            {
+                Vector3 newPos = Vector3.Normalize(player.transform.position - transform.position) * (speed / ((distance - 1) * (25 / attractionRadius)) + 1);
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
